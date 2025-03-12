ae3d188f80dd98a3d6abf9c0459d9ae2a0d54679
linoyniazov <linoy2209@gmail.com>
2025-03-12 18:08:18 +0200

    Added Swagger setup to server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,6 +8,8 @@ import postRoute from "./routes/post_route";
 import postInteraction from "./routes/post_interaction_route";
 import fileRoute from "./routes/file_route";
 import authRoute from "./routes/auth_route";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
 
 
 const app = express();
@@ -20,13 +22,32 @@ app.use("/file", fileRoute);
 app.use("/public", express.static("public"));
 app.use("/auth", authRoute);
 
+const options = {
+  definition: {
+  openapi: "3.0.0",
+  info: {
+  title: "Web Dev 2022 REST API",
+  version: "1.0.0",
+  description: "REST server including authentication using JWT",
+  },
+  servers: [{url: "http://localhost:5000",},],
+  },
+  apis: ["./src/routes/*.ts"],
+  };
+  const specs = swaggerJsDoc(options);
+  app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to database"));
+// const db = mongoose.connection;
+// db.on("error", (error) => console.error(error));
+// db.once("open", () => console.log("Connected to database"));
 
 const initApp = () => {
   return new Promise<Express>((resolve, reject) => {
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error:"));
+    db.once("open", function () {
+      console.log("Connected to the database");
+    });
     if (!process.env.MONGO_URI) {
       reject("MONGO_URI is not defined in .env file");
     } else {
