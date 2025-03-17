be9d776a1cca48c61d0e5fba75302a9791a6c1e3
linoyniazov <linoy2209@gmail.com>
2025-03-17 16:00:50 +0200

    fix: debug token persistence issue after navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,7 @@ import Layout from './components/Layout';
 import ProfilePage from './pages/ProfilePage';
 import UploadPost from './pages/UploadPost';
 import AuthForm from "./components/auth/AuthForm";
-import Registration from "./components/Registration"; // הו��פנו את הקו��פו��נ��ה של הרי��ו��
 
-
-// import Registration from "./components/Registration"; // הוספנו את הקומפוננטה של הרישום
-// import Login from './components/Login';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -17,13 +13,11 @@ function App() {
   return (
     <Router>
       <Routes>
+      <Route path="/" element={<AuthForm />} />
         <Route element={<Layout />}>
-          <Route path="/" element={<AuthForm />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/uploadPost" element={<UploadPost />} />
-          <Route path="/register" element={<Registration />} /> 
-          
+          <Route path="/uploadPost" element={<UploadPost />} />          
         </Route>
       </Routes>
     </Router>
